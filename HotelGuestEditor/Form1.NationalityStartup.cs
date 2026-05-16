4f8c9ca9af2c3de8c40d076c553c615d0f1809e0
AmmarNassar4 <ammar.nassar@outlook.com>
2026-05-16 22:08:46 +0300

    Initialize nationality dropdown on form load

---
+++ HotelGuestEditor/Form1.NationalityStartup.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HotelGuestEditor
+{
+    public partial class Form1
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            InitializeNationalityCombo();
+        }
+    }
+}
