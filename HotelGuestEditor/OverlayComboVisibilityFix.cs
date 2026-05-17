5685767a26e32f3e7f3332e8c8d1bce4691f570b
AmmarNassar4 <ammar.nassar@outlook.com>
2026-05-17 04:17:19 +0300

    Add overlay combo visibility helper

--- HotelGuestEditor/OverlayComboVisibilityFix.cs
+++ HotelGuestEditor/OverlayComboVisibilityFix.cs
@@ -2,5 +2,15 @@ namespace HotelGuestEditor
 {
     public partial class Form1
     {
+        private bool _overlayFixDone;
+
+        private void ApplyOverlayFixOnce()
+        {
+            if (_overlayFixDone) return;
+            _overlayFixDone = true;
+
+            lblTitle.VisibleChanged += (_, __) => { if (_cmbTitle != null) _cmbTitle.Visible = lblTitle.Visible; };
+            lblNationality.VisibleChanged += (_, __) => { if (_cmbNationality != null) _cmbNationality.Visible = lblNationality.Visible; };
+        }
     }
 }
